1bfbe57603b98f3202c84bf26a40bed1af3868f9
Ruvaim <ruvaim51@gmail.com>
2023-01-01 21:19:40 +0530

    Added script file

---
+++ script.js
@@ -0,0 +1,24 @@
+import File from './Models/File.js';
+import fs from 'fs';
+import connectDB from './config/db.js';
+
+connectDB();
+const deleteData = async () => {
+  const pastDate = new Date(Date.now() - 24 * 60 * 60 * 1000);
+  const files = await File.find({ createdAt: { $lt: pastDate } });
+  if (files.length) {
+    for (const file of files) {
+      try {
+        fs.unlinkSync(file.path);
+        await file.remove();
+        console.log(`Successfully deleted ${file.filename}`);
+      } catch (error) {
+        console.log(`Error while deleting file ${err}`);
+      }
+    }
+    console.log('Job Done!');
+  }
+};
+console.log('running');
+
+deleteData().then(process.exit);
